796457d76c417d915518415dbce4c6d010c931fa
Minh Tong <minhtt@msm.com.vn>
2025-07-26 12:49:18 +0700

    chore: exp Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { LoadScript } from '@react-google-maps/api';
+import { Analytics } from '@vercel/analytics/next';
 import App from './App'
 
 export const API_KEY = import.meta.env.VITE_API_KEY;
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <LoadScript googleMapsApiKey={API_KEY}>
         <App />
+        <Analytics />
       </LoadScript>
     </BrowserRouter>
   </StrictMode>,
