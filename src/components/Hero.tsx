5c70e8ff70c2cdfb472ef534ab4bd1b096008bf7
Minh Tong <minhtt@msm.com.vn>
2025-10-15 18:06:20 +0700

    feat: update digifarm page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,9 @@
 
 import Navigation from "./Navigation";
 
+import farmVideo from '../assets/videos/farm-vid.mp4'
+
+
 type HeroProps = {
   coverImage: string;
   icon?: React.FunctionComponent<any>;
@@ -16,11 +19,18 @@ export default function Hero({ coverImage, icon: Icon, title, description }: Her
         className="w-full md:h-50 h-32 bg-cover bg-center absolute top-0 z-0"
       >
         <div className="absolute bottom-0 md:h-34 h-20 w-full bg-gradient-to-b to-white from-white/0" />
+        <video
+          src={farmVideo}
+          autoPlay
+          loop
+          muted
+          className="w-full h-auto object-cover"
+        />
       </div>
       <div className="md:my-7 mt-8 mb-10 z-1">
         {Icon && <Icon className='w-40 h-12 md:w-auto md:h-auto md:max-w-70' />}
       </div>
-      <div className="md:h-20 h-0"/>
+      <div className="md:h-20 h-0" />
       {/* <Navigation /> */}
     </section>
   );
